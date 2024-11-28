521832211e6e7fbbec26806700dab0867475cdb4
AntonF45 <Kaliyga1@yandex.ru>
2024-11-28 22:28:25 +0500

    Add files via upload

--- module_10_3.py
+++ module_10_3.py
@@ -10,28 +10,50 @@ class Bank:
         self.lock = threading.Lock()
 
     def deposit(self):
+        counter = 0
         for i in range(100):
+            counter += 1
             amount = random.randint(50, 500)
             self.balance += amount
             print(f'Пополнение: {amount}. Баланс: {self.balance}')
 
+            """Проверяем условие: баланс больше или равен 500 (если не то пополняем до 500)
+             и заблокирован замок lock"""
+
+            if self.balance < 500:
+                # print(f'Цикл пополнения № {counter} завершен!')
+                continue
             if self.balance >= 500 and self.lock.locked():
                 self.lock.release()
 
+            # print(f'Цикл пополнения № {counter} завершен!')
             time.sleep(0.001)
 
     def take(self):
+        counter = 0
         for i in range(100):
+            counter += 1
             amount = random.randint(50, 500)
             print(f'Запрос на {amount }')
+
+            """Поверяем условие: 
+            - если случайное число меньше баланса - производим списание, иначе проверяем;
+            - если поток  deposit 'мертв' а цикл списания еще не завершился - пытаемся списать хоть что-нибудь;
+            - иначе открываем замок и пополняем баланс."""
 
             if amount <= self.balance:
                 self.balance -= amount
                 print(f'Снятие: {amount}. Баланс: {self.balance}')
             else:
-                print('Запрос отклонен, недостаточно средств.')
-                self.lock.acquire()
+                if not th1.is_alive() and counter < 100:
+                    print('Запрос отклонен, недостаточно средств.')
+                    # print(f'Цикл снятия № {counter} завершен!')
+                    continue
+                else:
+                    print('Запрос отклонен, недостаточно средств.')
+                    self.lock.acquire()
 
+            # print(f'Цикл снятия № {counter} завершен!')
             time.sleep(0.001)
 
 
